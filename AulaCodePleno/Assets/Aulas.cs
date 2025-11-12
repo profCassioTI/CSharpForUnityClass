27da9cb37195646925c088386c3da7bc0d470e7b
ProfessorHappy <ahappysantana@gmail.com>
2025-11-12 18:20:41 -0300

    Adicionou variáveis e fez comentários
    Os comentários são para explicar o uso dos tipos de variáveis

--- AulaCodePleno/Assets/Aulas.cs
+++ AulaCodePleno/Assets/Aulas.cs
@@ -1,14 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Aulas : MonoBehaviour
 {
+    //Tipos de Variáveis - 
+    //int - para valores inteiros
+    //float - para valores reais, ou seja, com virgula
+    //boll - para valores verdadeiro (true) ou falso (false)
+    //string - para escrever textos
     public int idade = 0;
     public float altura = 0;
     private int vida = 3;
     public int stamina = 4;
     public float velocidade = 5;
+    public bool vivo = true;
+    public string nome = "";
 
     // Start is called before the first frame update
     void Start()
