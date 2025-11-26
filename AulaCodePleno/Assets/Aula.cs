da84f69b6492d8d044149d5c89a82b99f64a8777
ProfessorHappy <ahappysantana@gmail.com>
2025-11-26 18:17:10 -0300

    Concluindo aula de variáveis e funções

--- AulaCodePleno/Assets/Aula.cs
+++ AulaCodePleno/Assets/Aula.cs
@@ -5,17 +5,37 @@ using UnityEngine;
 
 public class Aula : MonoBehaviour
 {
-    
+  #region   Aula de Variaveis e funções
+    //Tipos de Variáveis - 
+    //int - para valores inteiros
+    //float - para valores reais, ou seja, com virgula
+    //boll - para valores verdadeiro (true) ou falso (false)
+    //string - para escrever textos
+    public int idade = 15, vida = 3, stamina = 4;
+    public float altura = 1.75f, velocidade = 5f;
+    public bool vivo = true;
+    public string nome_presonagem = "João", nome_inimigo = "Gigante";
 
-
-    void Start()
+    public void Atacar()
     {
-        
+        if (vida > 0)
+        {
+            Debug.Log("Vida: " + vida +  "\n Atacando....");
+            
+        }
+    }
+#endregion
+
+
+   void Start()
+    {
+      
+       
     }
 
 
     void Update()
     {
-        Debug.Log("Olá Mundo");
+      
     }
 }
