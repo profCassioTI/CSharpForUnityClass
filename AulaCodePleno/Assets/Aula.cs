a929f2e5cec5048b07036eb6a570f7773ad2038f
ProfessorHappy <ahappysantana@gmail.com>
2025-11-26 17:04:07 -0300

    testando função void Update()

---
+++ AulaCodePleno/Assets/Aula.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aula : MonoBehaviour
+{
+    
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        Debug.Log("Olá Mundo");
+    }
+}
